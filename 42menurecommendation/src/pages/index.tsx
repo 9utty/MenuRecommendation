619e751146bd6b636d883fdf9f321c0212443e35
9utty <leegh4250@gmail.com>
2023-05-04 23:40:48 +0900

    feat: 불필요한 파일삭제

--- 42menurecommendation/src/pages/index.tsx
+++ 42menurecommendation/src/pages/index.tsx
@@ -1,43 +1,7 @@
-import { Col, Image, Row } from "antd";
-import React, { useState, useMemo } from "react";
-import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
-// UI components
-import {
-  MenuList,
-  MenuListItem,
-  Separator,
-  styleReset,
-  AppBar,
-  Frame,
-} from "react95";
-// Theme
-import original from "react95/dist/themes/original";
-import tokyoDart from "react95/dist/themes/tokyoDark";
-import App from "./_app";
-import {} from "./Appbar";
-import AppLayout from "./AppLayout";
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-`;
-
-const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-`;
-
-const Background = styled.div`
-  background-color: #008080;
-  width: 100vw;
-  height: 100vh;
-`;
+import React from "react";
+import AppLayout from "../components/AppLayout";
 
 const Home = () => {
-  const [keyword, setKeyword] = useState("");
-
   return (
     <div>
       <AppLayout>
